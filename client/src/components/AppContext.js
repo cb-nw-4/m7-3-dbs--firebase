b88d24cc13e7ca5ad1ecb43ac8ad2b3bfe90512b
Doug Wright <dw@nof49.ca>
2021-03-05 10:14:47 -0500

    Completed exercise 1

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -2,6 +2,15 @@ import React, { createContext, useEffect, useState } from 'react';
 
 export const AppContext = createContext(null);
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCvRLeMTytncXGfd2pKhsQlWqVljXGO9AY",
+  authDomain: "user-app-2071b.firebaseapp.com",
+  projectId: "user-app-2071b",
+  storageBucket: "user-app-2071b.appspot.com",
+  messagingSenderId: "832244567176",
+  appId: "1:832244567176:web:5f307b24220f588f44233d"
+};
+
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
